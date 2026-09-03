328e8cc3ed4953419a31be7fa26a987d29cba190
nom-d <75618727+n0m-d@users.noreply.github.com>
2026-09-03 22:38:22 +0545

    Signed

--- internal/uploads/signed_downloads.go
+++ internal/uploads/signed_downloads.go
@@ -1,6 +1,9 @@
 package uploads
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"strconv"
 	"strings"
@@ -15,14 +18,27 @@ func CreateSignedDownloadPath(signingKey [32]byte, fileID int64, now time.Time) 
 	return fmt.Sprintf("/files/%d/signed-download?expires=%d&signature=%s", fileID, expires, signature)
 }
 
-func VerifySignedDownload(_ [32]byte, _ int64, expiresValue, signature string, now time.Time) bool {
+func VerifySignedDownload(signingKey [32]byte, fileID int64, expiresValue, signature string, now time.Time) bool {
 	if expiresValue == "" || strings.Trim(expiresValue, "0123456789") != "" || len(signature) != 64 {
 		return false
 	}
 	expires, err := strconv.ParseInt(expiresValue, 10, 64)
-	return err == nil && expires > now.Unix()
+	if err != nil || expires <= now.Unix() {
+		return false
+	}
+	provided, err := hex.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+	expected, err := hex.DecodeString(signDownload(signingKey, fileID, expires))
+	if err != nil {
+		return false
+	}
+	return hmac.Equal(provided, expected)
 }
 
-func signDownload(_ [32]byte, _, _ int64) string {
-	return strings.Repeat("0", 64)
+func signDownload(signingKey [32]byte, fileID, expires int64) string {
+	mac := hmac.New(sha256.New, signingKey[:])
+	fmt.Fprintf(mac, "GET\n/files/%d/signed-download\n%d", fileID, expires)
+	return hex.EncodeToString(mac.Sum(nil))
 }
